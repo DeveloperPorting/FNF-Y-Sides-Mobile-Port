87957be47f684281fb7a32dddfac9d9032fcbd23
MrMadera <ismaelglhs@gmail.com>
2025-12-20 15:44:34 +0000

    week 4 effects and movements

--- source/states/stages/LimoStage.hx
+++ source/states/stages/LimoStage.hx
@@ -1,40 +1,73 @@
 package states.stages;
 
+import shaders.DropShadowShader;
+import shaders.WaterShader;
+import flixel.addons.display.FlxBackdrop;
 import states.stages.objects.*;
 import objects.Character;
 
 class LimoStage extends BaseStage
 {
+    var waterShader:WaterShader;
+
+    var sky:FlxSprite;
+    var clouds:FlxBackdrop;
+    var buildingsBack:FlxBackdrop;
+    var buildingsFront:FlxBackdrop;
+    var water:FlxSprite;
+    var road:FlxBackdrop;
+    var truck:FlxSprite;
+
 	override function create()
 	{
-        var sky:FlxSprite = new FlxSprite();
+        sky = new FlxSprite();
         sky.loadGraphic(Paths.image('sky'));
         add(sky);
 
-        var clouds:FlxSprite = new FlxSprite();
-        clouds.loadGraphic(Paths.image('clouds'));
+        clouds = new FlxBackdrop(Paths.image('clouds'), X, 20, 0);
         add(clouds);
 
-        var buildingsBack:FlxSprite = new FlxSprite(0, 200);
-        buildingsBack.loadGraphic(Paths.image('buildingBack'));
+        buildingsBack = new FlxBackdrop(Paths.image('buildingBack'), X, 0, 0);
+        buildingsBack.y = 200;
         add(buildingsBack);
 
-        var buildingsFront:FlxSprite = new FlxSprite(0, 200);
-        buildingsFront.loadGraphic(Paths.image('building'));
+        buildingsFront = new FlxBackdrop(Paths.image('building'), X, 0, 0);
+        buildingsFront.y = 200;
         add(buildingsFront);
 
-        var water:FlxSprite = new FlxSprite();
+        water = new FlxSprite();
         water.loadGraphic(Paths.image('water'));
         add(water);
 
-        var road:FlxSprite = new FlxSprite();
-        road.loadGraphic(Paths.image('road'));
+        if(ClientPrefs.data.shaders)
+        {
+            waterShader = new WaterShader();
+            waterShader.iTime.value = [0];
+            water.shader = waterShader;
+        }
+
+        road = new FlxBackdrop(Paths.image('road'), X, 0, 0);
         add(road);
 
-        var truck:FlxSprite = new FlxSprite();
+        truck = new FlxSprite();
         truck.loadGraphic(Paths.image('truck'));
         add(truck);
+
+        applyVelocites();
 	}
+
+    function applyVelocites()
+    {
+        clouds.velocity.set(20, 0);
+        buildingsBack.velocity.set(50, 0);
+        buildingsFront.velocity.set(75, 0);
+        road.velocity.set(3000, 0);
+    }
+
+    override function update(elapsed:Float)
+    {
+        waterShader.iTime.value[0] += elapsed;
+    }
 
     override function createPost() 
     {
@@ -42,5 +75,55 @@ class LimoStage extends BaseStage
         light.loadGraphic(Paths.image('light'));
         light.blend = ADD;
         add(light);
+
+		if(ClientPrefs.data.shaders)
+		{
+			// lights on characters
+			var rimBF = new DropShadowShader();
+			rimBF.setAdjustColor(0, 10, 0, 0);
+			rimBF.color = 0xFFFEF9AA;
+			game.boyfriend.shader = rimBF;
+			rimBF.attachedSprite = game.boyfriend;
+			rimBF.angle = 90;
+
+			game.boyfriend.animation.callback = function()
+			{
+				if (game.boyfriend != null)
+				{
+					rimBF.updateFrameInfo(game.boyfriend.frame);
+				}
+			};
+
+			var rimGF = new DropShadowShader();
+			rimGF.setAdjustColor(0, 10, 0, 0);
+			rimGF.color = 0xFFFEF9AA;
+			game.gf.shader = rimGF;
+			rimGF.attachedSprite = game.gf;
+			rimGF.distance = 10;
+			rimGF.angle = 90;
+
+			game.gf.animation.callback = function()
+			{
+				if (game.gf != null)
+				{
+					rimGF.updateFrameInfo(game.gf.frame);
+				}
+			};
+
+			var rimDad = new DropShadowShader();
+			rimDad.setAdjustColor(0, 10, 0, 0);
+			rimDad.color = 0xFFFEF9AA;
+			game.dad.shader = rimDad;
+			rimDad.attachedSprite = game.dad;
+			rimDad.angle = 90;
+
+			game.dad.animation.callback = function()
+			{
+				if (game.dad != null)
+				{
+					rimDad.updateFrameInfo(game.dad.frame);
+				}
+			};
+		}
     }
 }
