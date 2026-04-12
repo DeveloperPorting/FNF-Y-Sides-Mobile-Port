62e1cb47acadfae895c33611b1c77ef069025f0e
MrMadera <90923803+MrMadera@users.noreply.github.com>
2026-04-12 22:49:07 +0100

    the damn font

--- source/states/NewStoryMenuState.hx
+++ source/states/NewStoryMenuState.hx
@@ -1,13 +1,26 @@
 package states;
 
+import flixel.graphics.frames.FlxBitmapFont;
+import flixel.text.FlxBitmapText;
+
 class NewStoryMenuState extends MusicBeatState
 {
 	public static var weekCompleted:Map<String, Bool> = new Map<String, Bool>();
 	public static var backFromStoryMode:Bool = false;
 
+	var songTextTemp:FlxBitmapText;
+
     override function create()
     {
         super.create();
+
+		var fontLetters:String = "abcgipydefhjqzklmnorstuvwx";
+		songTextTemp = new FlxBitmapText(FlxBitmapFont.fromMonospace(Paths.image("storymenu/new/storyfont"), fontLetters, FlxPoint.get(42, 56)));
+		songTextTemp.text = "";
+		songTextTemp.antialiasing = ClientPrefs.data.antialiasing;
+        songTextTemp.text = 'hola gbv princesa';
+        songTextTemp.screenCenter();
+        add(songTextTemp);
     }
 
     override function update(elapsed:Float)
