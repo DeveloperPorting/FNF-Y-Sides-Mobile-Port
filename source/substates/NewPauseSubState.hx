22372c7089071ea2c42c4dff2ceccf49d266e286
StarNovaBR <cherrylegal001@gmail.com>
2026-07-11 09:45:32 -0300

    a

--- source/substates/NewPauseSubState.hx
+++ source/substates/NewPauseSubState.hx
@@ -286,14 +286,14 @@ class NewPauseSubState extends MusicBeatSubstate
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
 		}
-		#if mobile controls.isInSubstate = false; #end
+		#if mobile backend.Controls.instance.isInSubstate = false; #end
 		MusicBeatState.resetState();
 	}
 
     function closePauseMenu()
     {
 			pauseMusic.stop();
-			#if mobile controls.isInSubstate = false; #end
+			#if mobile backend.Controls.instance.isInSubstate = false; #end
             close();
     }
 }
