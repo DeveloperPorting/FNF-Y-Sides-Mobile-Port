ecd6cd1e3a428d68782f18a7c05fc748314afc6a
StarNovaBR <cherrylegal001@gmail.com>
2026-07-12 17:09:38 -0300

    minha utima tentativa

--- source/shaders/DeflectiveLens.hx
+++ source/shaders/DeflectiveLens.hx
@@ -7,71 +7,66 @@ class DeflectiveLens extends FlxShader
     @glFragmentSource('
 	#pragma header
 	
-	#define round(a) floor(a + 0.5)
-	#define iResolution vec3(openfl_TextureSize, 0.0)
 	uniform float iTime;
-	#define iChannel0 bitmap
 	uniform sampler2D iChannel1;
 	uniform sampler2D iChannel2;
 	uniform sampler2D iChannel3;
-	#define texture flixel_texture2D
-	
-	// variables which is empty, they need just to avoid crashing shader
 	uniform float iTimeDelta;
 	uniform float iFrameRate;
 	uniform int iFrame;
-	#define iChannelTime iTime
-	#define iChannelResolution iResolution
 	uniform vec4 iMouse;
 	uniform vec4 iDate;
-	
-	const float pi = 3.14159265358979323846;
-	const float epsilon = 1e-6;
-	
-	const float fringeExp = 2.3;
-	const float fringeScale = 0.0;
-	const float distortionExp = 2.0;
 	uniform float distortionScale;
 	
-	const float startAngle = 1.23456 + pi;	// tweak to get different fringe colouration
-	const float angleStep = pi * 2.0 / 3.0;	// space samples every 120 degrees
+	#define round(a) floor(a + 0.5)
+	#define iResolution vec3(openfl_TextureSize, 0.0)
+	#define iChannel0 bitmap
+	#define texture flixel_texture2D
+	#define iChannelTime iTime
+	#define iChannelResolution iResolution
+	
+	const float pi = 3.14159265358979323846;
+	const float fringeExp = 2.3;
+	const float fringeScale = 0.0; 
+	const float distortionExp = 2.0;
+	
+	const float startAngle = 4.37615265; // 1.23456 + pi
+	const float angleStep = 2.0943951;   // pi * 2.0 / 3.0
 	
 	void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	{
 	    vec2 baseUV = fragCoord.xy / iResolution.xy;
-	    vec2 fromCentre = baseUV - vec2(0.5, 0.5);
+	    vec2 fromCentre = baseUV - 0.5;
 	    
-	    // correct for aspect ratio
 	    fromCentre.y *= iResolution.y / iResolution.x;
 	    float radius = length(fromCentre);
-	    fromCentre = radius > epsilon
-	        ? (fromCentre * (1.0 / radius))
-	        : vec2(0.0);
 	    
 	    float strength = 0.5 - (iMouse.x / iResolution.x);
-	    float rotation = iMouse.y / iResolution.y * 2.0 * pi;
+	    
+	    vec2 distortUV = baseUV - fromCentre * (distortionScale * radius * strength);
 	    
 	    float fringing = fringeScale * pow(radius, fringeExp) * strength;
-	    float distortion = distortionScale * pow(radius, distortionExp) * strength;
 	    
-	    vec2 distortUV = baseUV - fromCentre * distortion;
+	    if (fringing == 0.0) {
+	        fragColor = texture(iChannel0, distortUV);
+	        return;
+	    }
 	    
-	    float angle;
-	    vec2 dir;
+	    float rotation = (iMouse.y / iResolution.y) * 6.2831853; // 2.0 * pi
+	    float angle = startAngle + rotation;
 	    
-	    angle = startAngle + rotation;
-	    dir = vec2(sin(angle), cos(angle));
-	    vec4 redPlane = texture(iChannel0, distortUV + fringing * dir);
+	    vec2 dir = vec2(sin(angle), cos(angle));
+	    float r = texture(iChannel0, distortUV + fringing * dir).r;
 	    
 	    angle += angleStep;
 	    dir = vec2(sin(angle), cos(angle));
-	    vec4 greenPlane = texture(iChannel0, distortUV + fringing * dir);
+	    float g = texture(iChannel0, distortUV + fringing * dir).g;
 	    
 	    angle += angleStep;
 	    dir = vec2(sin(angle), cos(angle));
 	    vec4 bluePlane = texture(iChannel0, distortUV + fringing * dir);
 	    
-	    fragColor = vec4(redPlane.r, greenPlane.g, bluePlane.b, texture(iChannel0, distortUV).a);
+	    fragColor = vec4(r, g, bluePlane.b, bluePlane.a);
 	}
 	
 	void main() {
